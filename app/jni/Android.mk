46e974a231aed5c8e04e59334ffe3050e75a86a9
jricon <jrincon@dsic.upv.es>
2019-04-03 15:17:10 +0200

    first commit

---
+++ app/jni/Android.mk
@@ -0,0 +1,10 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+OPENCV_INSTALL_MODULES:=on
+OPENCV_LIB_TYPE:=SHARED
+include /Users/jaimerinconarango/Dropbox/Hacksterio_Projects/RICOH_CameraTHETA_V_CV/OpenCV-android-sdk/sdk/native/jni/OpenCV.mk
+LOCAL_MODULE := opencvsample
+LOCAL_SRC_FILES := sample.cpp
+include $(BUILD_SHARED_LIBRARY)
